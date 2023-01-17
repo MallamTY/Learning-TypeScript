e145b0547de8f2e0ca9b098a3efede21410ed068
MallamTY <sosanyatemitayonurudeen@gmail.com>
2023-01-17 15:58:33 +0100

    More on class with typescript

--- app.js
+++ app.js
@@ -45,6 +45,62 @@ var ICT = /** @class */ (function (_super) {
     }
     return ICT;
 }(Department));
-var dept = new Department('HQ', 'John Oyegunle', 'To be the best brewery in the Opco');
-var ict = new ICT(['MallamTY', 'Temitayo'], 'Temitayo Sosanya', 'To get our packets to reach their destination .....');
-ict.description();
+var Accouting = /** @class */ (function (_super) {
+    __extends(Accouting, _super);
+    function Accouting(kpi) {
+        var _this = _super.call(this, 'Accounting', 'Temitayo Sosanya', kpi) || this;
+        _this.report = [];
+        _this.kpi = kpi;
+        return _this;
+    }
+    Object.defineProperty(Accouting.prototype, "mostRecentReport", {
+        get: function () {
+            if (this.latestReport) {
+                return this.latestReport;
+            }
+            throw Error('No report found !!!!!!!!!!');
+        },
+        set: function (content) {
+            if (this.report.length < 3) {
+                this.addReport(content);
+            }
+            else {
+                throw Error('You already have more than 3 reports in your database');
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Accouting.prototype.addReport = function (content) {
+        this.report.push(content);
+        this.latestReport = this.report[0];
+    };
+    Accouting.prototype.genReport = function () {
+        console.log(this.report);
+    };
+    Accouting.prototype.genLastReport = function () {
+        console.log(this.latestReport);
+    };
+    Accouting.prototype.deletLastReport = function () {
+        if (this.report.length !== 0) {
+            this.report.pop();
+            console.log('Last report successfully removed from the report list');
+        }
+        else {
+            console.log('Your report list is currently empty !!!!!!!!!!!!');
+        }
+    };
+    Accouting.yearOfEstablishment = 2007;
+    return Accouting;
+}(Department));
+// const dept = new Department('HQ', 'John Oyegunle', 'To be the best brewery in the Opco')
+// const ict = new ICT(['MallamTY', 'Temitayo'], 'Temitayo Sosanya', 'To get our packets to reach their destination .....');
+// ict.description()
+var accounting = new Accouting('To deliver a world class accouting servie');
+accounting.addReport('We delivered the best 3 accouting service in the last 3 months');
+accounting.addReport('We won the best accouting firm for the year 2015');
+accounting.mostRecentReport = ('We won the best accouting firm for the year 2016');
+console.log(Accouting.yearOfEstablishment);
+console.log(accounting);
+accounting.deletLastReport();
+// console.log(accounting);
--- app.ts
+++ app.ts
@@ -34,9 +34,67 @@ class ICT extends Department {
     }
 }
 
-const dept = new Department('HQ', 'John Oyegunle', 'To be the best brewery in the Opco')
-const ict = new ICT(['MallamTY', 'Temitayo'], 'Temitayo Sosanya', 'To get our packets to reach their destination .....');
-ict.description()
+class Accouting extends Department {
+    private latestReport: string;
+    private report: string [] = [];
+    static yearOfEstablishment = 2007;
 
+    get mostRecentReport () {
+        if (this.latestReport) {
+            return this.latestReport
+        }
+        throw Error('No report found !!!!!!!!!!')
+    }
 
+    set mostRecentReport(content: string) {
+        if (this.report.length < 3) {
+            this.addReport(content)
+        }
+        else {
+            throw Error ('You already have more than 3 reports in your database')
+        }
+    }
 
+    constructor (kpi: string){
+        super ('Accounting', 'Temitayo Sosanya', kpi);
+        this.kpi = kpi;
+
+    }
+    addReport(content: string) {
+        this.report.push(content)
+        this.latestReport = this.report[0]
+    }
+
+    genReport(){
+        console.log(this.report);
+        
+    }
+    
+    genLastReport(){
+        console.log(this.latestReport);
+        
+    }
+    deletLastReport(){
+        if(this.report.length !== 0){
+            this.report.pop()
+            console.log('Last report successfully removed from the report list');
+        }
+        else {
+            console.log('Your report list is currently empty !!!!!!!!!!!!');
+            
+        }
+    }
+}
+
+// const dept = new Department('HQ', 'John Oyegunle', 'To be the best brewery in the Opco')
+// const ict = new ICT(['MallamTY', 'Temitayo'], 'Temitayo Sosanya', 'To get our packets to reach their destination .....');
+// ict.description()
+const accounting = new Accouting('To deliver a world class accouting servie')
+accounting.addReport('We delivered the best 3 accouting service in the last 3 months')
+accounting.addReport('We won the best accouting firm for the year 2015')
+accounting.mostRecentReport = ('We won the best accouting firm for the year 2016')
+console.log(Accouting.yearOfEstablishment);
+
+console.log(accounting);
+accounting.deletLastReport()
+// console.log(accounting);
